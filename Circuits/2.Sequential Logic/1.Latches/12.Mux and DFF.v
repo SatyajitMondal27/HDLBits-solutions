3043346644473dfe872dea35de93184c0ee437d1
SatyajitMondal27 <124804860+SatyajitMondal27@users.noreply.github.com>
2023-10-17 00:38:08 +0530

    Update 12.Mux and DFF.v with new code written using dff and mux submodules

--- Circuits/2.Sequential Logic/1.Latches/12.Mux and DFF.v
+++ Circuits/2.Sequential Logic/1.Latches/12.Mux and DFF.v
@@ -4,26 +4,34 @@ including both the flip-flop and multiplexers.*/
 module top_module (
     input clk,
     input w, R, E, L,
-    output reg Q
+    output Q
 );
-    wire w0,w1;
+    wire w1,w2;
+    mux one(Q,w,E,w1);
+    mux two(w1,R,L,w2);
+    dflip save(clk,w2,Q);
+endmodule
+
+
+module mux(input a,b,s,output o);
     always@(*)
         begin
-            if(E==0)
-                w0=Q;
+            if(s==0)
+                o=a;
             else
-                w0=w;
-        end
-    always@(*)
-        begin
-            if(L==0)
-                w1=w0;
-            else
-                w1=R;
-        end
+                o=b;
+        end        
+endmodule
+
+module dflip(
+    input clk,
+    input d,   // synchronous reset
+    output q);
     always@(posedge clk)
         begin
-            Q<=w1;
+            if(clk==1)
+                q<=d;
+            else
+                q<=q;
         end
-
 endmodule
